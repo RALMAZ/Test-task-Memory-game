de4c784b6f042056c7bad76cd594e5859216f0bf
Roman ALmazov <ralmaz.vue@ya.ru>
2018-08-29 11:25:58 +0300

    Add vuesax and cards

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,36 @@
 <template>
   <div id="app">
+    <vs-row vs-w="12">
+      <vs-col vs-type="flex" vs-justify="center" vs-align="center" vs-w="12">
+        <img src="../src/assets/header.png">
+      </vs-col>
+    </vs-row>
+
+    <vs-row vs-w="12">
+      
+      <vs-col vs-type="flex" vs-justify="center" vs-align="center" vs-w="2">    
+        <vs-card actionable>
+          <vs-card-body>
+            <span>Lorem ipsum dolor sit amet, consectetur adipiscing elit</span>
+          </vs-card-body>
+        </vs-card>
+      </vs-col>
+
+    </vs-row>
+
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+<style>
+  html {
+    background-color:#292e2a;
   }
-}
+  .vs-card {
+    background-color:white;
+  }
+  #app {
+    max-width:1000px;
+    margin: 0 auto;
+    padding: 1.5rem 1.5rem;
+  }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 import Vue from 'vue'
-import App from './App.vue'
+import Vuesax from 'vuesax'
+import 'vuesax/dist/vuesax.css'
+Vue.use(Vuesax)
+
 import store from './store'
+import App from './App.vue'
 
 Vue.config.productionTip = false
 
