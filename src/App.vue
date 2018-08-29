a28ca51c8ac7783e3e379e9f695c7c3d35c95bdb
Roman ALmazov <ralmaz.vue@ya.ru>
2018-08-29 13:10:32 +0300

    Ready

--- src/App.vue
+++ src/App.vue
@@ -9,34 +9,47 @@
     </vs-row>
 
     <vs-row vs-w="12">
-
-      <vs-col
-        v-for="(item, index) in cards" :key="index"
-        vs-type="flex"
-        vs-justify="center"
-        vs-align="center"
-        vs-w="2"
-      >
-        <vs-card
-          actionable
-          @vs-click="item.show = true"
+        <vs-col
+          v-for="(item, index) in dataCard" :key="index"
+          vs-type="flex"
+          vs-justify="center"
+          vs-align="center"
+          vs-w="2"
         >
-          <vs-card-body>
-            <span
-              v-if="!item.show"
-            >
-              <img
-                :src="require(`@/assets/${item.img}`)"
-                class="img"
-                alt="Card"
+          <vs-card
+            v-if="!item.active"
+            actionable
+            @vs-click="item.show = true; initCard(item.id); activeCard(); hideCard();"
+          >
+            <vs-card-body>
+              <span
+                v-if="item.show"
               >
-            </span>
-            <span v-else>
-              Что у нас тут?
-            </span>
-          </vs-card-body>
-        </vs-card>
-      </vs-col>
+                <img
+                  :src="require(`@/assets/${item.img}`)"
+                  class="img"
+                  alt="Card"
+                >
+              </span>
+              <span
+                v-else
+              >
+                Что у нас тут?
+              </span>
+            </vs-card-body>
+          </vs-card>
+          <vs-card v-else>
+            <vs-card-body>
+              <span>
+                <img
+                  :src="require(`@/assets/${item.img}`)"
+                  class="img"
+                  alt="Card"
+                >
+              </span>
+            </vs-card-body>
+          </vs-card>
+        </vs-col>
 
     </vs-row>
 
@@ -51,7 +64,7 @@
     data() {
       return {
         current: [],
-        initCards: []
+        dataCard: []
       }
     },
     computed: mapState({
@@ -59,12 +72,66 @@
       moves: state => state.moves,
       cards: state => state.cards
     }),
-    mounted() {
-      this.initCards = this.cards;
-      for (let index = 0; index < this.initCards.length; index++) {
-        const element = this.initCards[index];
-        element.show = false;
+    methods: {
+      initCard(id) {
+        this.current[this.current.length] = id;
+      },
+      hideCard() {
+        if (this.current.length > 2) {
+          let idCardHide = this.current[0];
+          this.current.splice(0, 1);
+
+          for (let index = 0; index < this.dataCard.length; index++) {
+            let element = this.dataCard[index];
+            if (idCardHide == element.id) {
+              element.show = false;
+            }
+          }
+        }
+      },
+      shuffleCards(array) {
+        for (var i = array.length - 1; i > 0; i--) {
+            var j = Math.floor(Math.random() * (i + 1));
+            var temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
+        }
+        return array;
+      },
+      activeCard() {
+        let id = this.current[0];
+        let id2 = this.current[1];
+
+        let test = Number(id) - Number(15);
+        let test2 = Number(id) + Number(15);
+        let test3 = Number(id2) - Number(15);
+        let test4 = Number(id2) + Number(15);
+          
+        for (let i = 0; i < this.dataCard.length; i++) {
+          let el = this.dataCard[i];
+          let ready = false;
+          let ready2 = false;
+
+          if (el.id == id) {
+            if (test == id2 || test2 == id2) {
+              ready = true;
+            }
+          }
+
+          if (el.id == id2) {
+            if (test3 == id || test4 == id) {
+              ready2 = true;
+            }
+          }
+
+          if (ready || ready2) {
+            this.dataCard[i].active = true;
+          }
+        }
       }
+    },
+    mounted() {
+      this.dataCard = this.shuffleCards(this.cards);
     }
   }
 </script>
@@ -75,6 +142,7 @@
   }
   .vs-card {
     background-color:white;
+    min-height: 200px;
   }
   .img {
     width: 100%;
--- src/store.js
+++ src/store.js
@@ -10,63 +10,183 @@ export default new Vuex.Store({
     cards: [
       {
         id: 1,
-        img: '1.png'
+        img: '1.png',
+        show: false,
+        active: false
       },
       {
         id: 2,
-        img: '2.png'
+        img: '2.png',
+        show: false,
+        active: false
       },
       {
         id: 3,
-        img: '3.png'
+        img: '3.png',
+        show: false,
+        active: false
       },
       {
         id: 4,
-        img: '4.png'
+        img: '4.png',
+        show: false,
+        active: false
       },
       {
         id: 5,
-        img: '5.png'
+        img: '5.png',
+        show: false,
+        active: false
       },
       {
         id: 6,
-        img: '6.png'
+        img: '6.png',
+        show: false,
+        active: false
       },
       {
         id: 7,
-        img: '7.png'
+        img: '7.png',
+        show: false,
+        active: false
       },
       {
         id: 8,
-        img: '8.png'
+        img: '8.png',
+        show: false,
+        active: false
       },
       {
         id: 9,
-        img: '9.png'
+        img: '9.png',
+        show: false,
+        active: false
       },
       {
         id: 10,
-        img: '10.png'
+        img: '10.png',
+        show: false,
+        active: false
       },
       {
         id: 11,
-        img: '11.png'
+        img: '11.png',
+        show: false,
+        active: false
       },
       {
         id: 12,
-        img: '12.png'
+        img: '12.png',
+        show: false,
+        active: false
       },
       {
         id: 13,
-        img: '13.png'
+        img: '13.png',
+        show: false,
+        active: false
       },
       {
         id: 14,
-        img: '14.png'
+        img: '14.png',
+        show: false,
+        active: false
       },
       {
         id: 15,
-        img: '15.png'
+        img: '15.png',
+        show: false,
+        active: false
+      },
+      {
+        id: 16,
+        img: '1.png',
+        show: false,
+        active: false
+      },
+      {
+        id: 17,
+        img: '2.png',
+        show: false,
+        active: false
+      },
+      {
+        id: 18,
+        img: '3.png',
+        show: false,
+        active: false
+      },
+      {
+        id: 19,
+        img: '4.png',
+        show: false,
+        active: false
+      },
+      {
+        id: 20,
+        img: '5.png',
+        show: false,
+        active: false
+      },
+      {
+        id: 21,
+        img: '6.png',
+        show: false,
+        active: false
+      },
+      {
+        id: 22,
+        img: '7.png',
+        show: false,
+        active: false
+      },
+      {
+        id: 23,
+        img: '8.png',
+        show: false,
+        active: false
+      },
+      {
+        id: 24,
+        img: '9.png',
+        show: false,
+        active: false
+      },
+      {
+        id: 25,
+        img: '10.png',
+        show: false,
+        active: false
+      },
+      {
+        id: 26,
+        img: '11.png',
+        show: false,
+        active: false
+      },
+      {
+        id: 27,
+        img: '12.png',
+        show: false,
+        active: false
+      },
+      {
+        id: 28,
+        img: '13.png',
+        show: false,
+        active: false
+      },
+      {
+        id: 29,
+        img: '14.png',
+        show: false,
+        active: false
+      },
+      {
+        id: 30,
+        img: '15.png',
+        show: false,
+        active: false
       }
     ]
   }
